f88ae483b8d7d4c46b894cc47f528aec6c8488ed
yujitokutake <youtok2019@gmail.com>
2019-04-09 17:14:20 +0900

    add php 2file

---
+++ check.php
@@ -0,0 +1,88 @@
+﻿<?php
+//入力フォームから送信されてきたデータをPHPが受け取り、各変数へ代入、画面表示へ利用する。
+	$yourname=htmlspecialchars($_REQUEST['yourname'], ENT_QUOTES);
+
+	$tel=mb_convert_kana(
+	htmlspecialchars($_REQUEST['tel1'], ENT_QUOTES).'-'.
+	htmlspecialchars($_REQUEST['tel2'], ENT_QUOTES).'-'.
+	htmlspecialchars($_REQUEST['tel3'], ENT_QUOTES), 'a', 'UTF-8');
+	
+	$zip=mb_convert_kana(
+	htmlspecialchars($_REQUEST['post1'], ENT_QUOTES).'-'.
+	htmlspecialchars($_REQUEST['post2'], ENT_QUOTES), 'a', 'UTF-8');
+
+	$todoufuken=htmlspecialchars($_REQUEST['todoufuken'], ENT_QUOTES);
+    $jyusho=htmlspecialchars($_REQUEST['jyusho'], ENT_QUOTES);
+	$koumei=htmlspecialchars($_REQUEST['koumei'], ENT_QUOTES);
+	$question=htmlspecialchars($_REQUEST['question'], ENT_QUOTES);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>講座のお申し込み確認</title>
+<style>
+	span{
+	color:red;
+	}
+</style>
+</head>
+<body>
+<fieldset>
+<legend><strong>講座のお申し込み確認</strong></legend>
+<p><?php print($yourname) ?>様の登録内容は、下記の内容でよろしいでしょうか？</p>
+<dl>
+	<dt>氏名：</dt>
+	<dd>
+		<?php
+			if($yourname == ""){
+				print '<span>入力されていません！</span>';
+			}else{
+				print($yourname) ;
+			}	
+		?>
+	<dd>
+	<dt>電話：</dt>
+	<dd>
+		<?php
+			if($tel == 0){
+				print '<span>入力されていません！</span>';
+			}else{
+				print($tel) ;
+			}	
+		?>
+	</dd>
+	<dt>郵便番号：</dt>
+	<dd><?php print($zip) ?></dd>
+	<dt>都道府県：</dt>
+	<dd><?php print($todoufuken) ?></dd>
+	<dd><?php print($jyusho) ?></dd>
+	<dt>受講校名：</dt>
+	<dd><?php print($koumei) ?></dd>
+	<dt>受講コース：</dt>
+	<dd>
+	<?php
+		foreach((array)$_REQUEST['course'] as $course) {
+			print(htmlspecialchars($course, ENT_QUOTES).'<br/>');
+		}  
+	 ?>
+	</dd>
+	<dt>きっかけ：</dt>
+	<dd>
+	<?php
+		foreach((array)$_REQUEST['chance'] as $chance) {
+			print(htmlspecialchars($chance, ENT_QUOTES).'<br/>');
+		}
+	?>
+	</dd>
+	<dt>ご意見・ご感想：</dt>
+	<dd><?php print(htmlspecialchars($_REQUEST['question'], ENT_QUOTES)) ?></dd>
+	<br />
+</dl>
+<p>
+	<input type="submit" name="btn1" id="btn1" value="確定">
+	<input type="button" name="btn2" id="btn2" value="修正" onClick="history.back();">
+</p>
+</fieldset>
+</body>
+</html>
---
+++ form2b.php
@@ -0,0 +1,126 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>フォーム2b</title>
+</head>
+
+<body>
+<form action="check.php" method="post">
+<table width="500" border="1">
+<caption>
+講座のお申し込みについて
+</caption>
+ <tr>
+  <th>お名前</th>
+  <td>
+   <input type="text" id="yourname" name="yourname" size="20" maxlength="20">
+  </td>
+ </tr>
+ 
+ <tr>
+  <th>お電話番号</th>
+  <td>
+   <input type="text" name="tel1" id="tel1" size="4" maxlength="4">
+   -
+   <input type="text" name="tel2" id="tel2" size="3" maxlength="3">
+   -
+   <input type="text" name="tel3" id="tel3" size="4" maxlength="4">
+  </td>
+ </tr>
+ 
+ <tr>
+  <th>ご住所</th>
+  <td>郵便番号
+   <input type="text" name="post1" id="post1" size="3" maxlength="3">
+   -
+   <input type="text" name="post2" id="post1" size="4" maxlength="4">
+  </td>
+ </tr>
+ <tr>
+  <th>都道府県</th>
+  <td>
+   <select name="todoufuken" id="todoufuken">
+    <option value="奈良">奈良</option>
+    <option value="大阪">大阪</option>
+    <option value="兵庫">兵庫</option>
+    <option value="和歌山">和歌山</option>
+    <option value="滋賀">滋賀</option>
+    <option value="三重">三重</option>
+    <option value="京都">京都</option>
+   </select>
+ </td>
+ </tr>
+
+ <tr>
+  <th>それ以降</th>
+  <td>
+   <input type="text" name="jyusho" id="jyusho" size="30">
+  </td>
+ </tr>
+
+<tr>
+  <th>受講校名</th>
+  <td>
+   <label>  
+    <input type="radio" name="koumei" value="鶴橋校"checked="checked">鶴橋校
+   </label>
+   <label>  
+    <input type="radio" name="koumei" value="布施校">布施校
+   </label>
+   <label>  
+    <input type="radio" name="koumei" value="上本町校">上本町校
+   </label>
+  </td>
+ </tr>
+
+<tr><th>受講コース<br>※複数選択可</th>
+<td>
+  <label>  
+    <input type="checkbox" name="course[]" value="Word"
+    checked="checked">Word
+  </label>
+  <label>  
+    <input type="checkbox" name="course[]" value="Excel">Excel
+  </label>
+  <label>
+    <input type="checkbox" name="course[]" value="PowerPoint">PowerPoint
+  </label>
+  <label>  
+    <input type="checkbox" name="course[]" value="Access">Access
+  </label>
+</td>
+</tr>
+
+<tr>
+  <th>知ったきっかけ<br />※複数選択可</th>
+  <td>
+   <select name="chance[]" size="4" multiple="multiple">
+    <option value="新聞">新聞</option>
+    <option value="テレビ">テレビ</option>
+    <option value="ラジオ">ラジオ</option>
+    <option value="雑誌">雑誌</option>
+   </select>
+  </td>
+</tr>
+
+<tr>
+  <th>ご質問</th>
+  <td>
+   <textarea name="question" id="question" cols="45" rows="5"></textarea>
+  </td>
+ </tr>
+ 
+
+ <tr>
+  <th>
+   <input type="submit" name="soushin_btn" id="soushin_btn" value="送信">
+  </th>
+  <td>
+   <input type="reset" name="reset_btn" id="reset_btn" value="リセット">
+  </td>
+ </tr>
+</table>
+</form>
+</body>
+</html>
